adaa00dfc54bda62e300462d21e59fc8b2693885
NSquare25 <karnissan2007@gmail.com>
2025-05-15 12:36:31 +0530

    Delete streamlit.py

--- streamlit.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.title("NKSTUDIOS")
-st.header("Imagine to Inspire; Implement to Immortalise")
-st.subheader("Home of music by NKBEATZ, movies and web-series by NKFILMS, stories and novels by NKPUBLISHERS")
