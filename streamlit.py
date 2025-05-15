b1fe1c60a60bee0968d942ed55e00cb45f56d05a
NSquare25 <karnissan2007@gmail.com>
2025-05-15 12:37:11 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("NKSTUDIOS")
+st.header("Imagine to Inspire; Implement to Immortalise")
+st.subheader("Home of music by NKBEATZ, movies and web-series by NKFILMS, stories and novels by NKPUBLISHERS")
+st.markdown("Official Logo: ")
+st.image("NKSTUDIOS Official Logo.png")
