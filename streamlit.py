65a23a97955b2d4e93d041d5ad3419e7aa77838e
NSquare25 <karnissan2007@gmail.com>
2025-05-15 13:16:15 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,6 +9,6 @@ st.radio("Pick your gender",['Male','Female','Transgender','Prefer not to say'])
 st.slider("Pick your age", 18, 100)
 st.multiselect("From where did you get to know about us?",['Social Media','Current Employees','Newspaper','Television','Prefer not to say'])
 st.multiselect("You are applying for:",['Internship','Contract-based Work','Permanent Job'])
+idd = random.randrange(1000000,2000000)
 if st.button("Submit"):
-    st.write("Hello,{name}! Thanks for applying. Welcome to NKSTUDIOS. Your ID is: ", random.randint[1000000,2000000])
-    
+    st.write("Hello,{name}! Thanks for applying. Welcome to NKSTUDIOS. Your ID is: ",idd)
